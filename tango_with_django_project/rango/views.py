24d3a992f9947a0aa0f133b121e06daa3169ea0e
Edward Samuel Richards <2405040r@student.gla.ac.uk>
2019-01-25 12:34:32 +0000

    .gitignore included and working as intended

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
+from django.shortcuts import render 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
-
+    context_dict = {'boldmessage':"Crunchy, creamy. cookie, candy, cupcake!"}
+    # ^ template vars
+    # !^ return rendered template to client
+    return render(request, 'rango/index.html', context=context_dict)
+	
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
